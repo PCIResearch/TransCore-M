99b370e7b5ff8fed8fe37fe169356b10e1311fd5
zhangdaowei <zhangdaowei2018@gmail.com>
2024-01-16 14:24:33 +0800

    fix: load model

--- transcorem/model/multimodal_encoder/builder.py
+++ transcorem/model/multimodal_encoder/builder.py
@@ -7,11 +7,11 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     if config_param.vision_model_path:
         vision_tower = config_param.vision_model_path
 
-    if not os.path.isabs(vision_tower) and config_param.model_path and not vision_tower.startswith("openai") and not vision_tower.startswith("laion"):
+    if not os.path.isabs(vision_tower) and config_param.model_path and not vision_tower.startswith("openai") and not vision_tower.startswith("laion") and not vision_tower.startswith("PCIResearch"):
         vision_tower = os.path.join(config_param.model_path, vision_tower)
 
     is_absolute_path_exists = os.path.exists(vision_tower)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
+    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or vision_tower.startswith("PCIResearch"):
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
 
     raise ValueError(f'Unknown vision tower: {vision_tower}')
